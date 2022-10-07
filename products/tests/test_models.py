e6ca570a5b32cb2b59ae121cfd2ccafceae14390
chapsvictor <adebayovictordare@yahoo.com>
2022-10-07 16:21:54 +0100

    update

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -1,69 +1,84 @@
 from django.utils.text import slugify
 import unittest
+from django.test import TestCase
 from userapp.models import User
 from ..models import *
 
 
-class TestCategory(unittest.TestCase):
-    name="testecommerce category"
-    slug= slugify(name)
-    category=Category.objects.create(name=name, slug=slug)
+class TestCategory(TestCase):
+    def setUp(self):
+
+        self.name="testecommerce category"
+        self.slug= slugify(self.name)
+        self.category=Category.objects.create(name=self.name, slug=self.slug)
 
     def test_category_creation(self):
         self.assertEqual(self.category.name, "testecommerce category")
         self.assertEqual(self.category.slug, "testecommerce-category")
         
         
-    category.delete()
+class TestColour(TestCase):
+    def setUp(self):
+
+        self.name="testcolour"
+       
+
+    def test_colour_creation(self):
+        colour=Colour.objects.create(name=self.name)
+
+        self.assertEqual(colour.name, "testcolour")
+            
+
+class TestProduct(TestCase):
+
+    def setUp(self):
+
+        self.email = 'testemail@yahoo.com'
+        self.username = 'testemail'
+        self.first_name = 'testfirstname'
+        self.address = "testaddress"
+        self.contact = "0903249609"
+        self.password ='12345'
+
+        self.category_name = "testecommerce category"
+        self.colour = "testcolour"
 
 
-class TestProduct(unittest.TestCase):
-
-    email = 'testemail@yahoo.com'
-    username = 'testemail'
-    first_name = 'testfirstname'
-    address="testaddress"
-    contact="0903249609"
-    password='12345'
-
-
-    test_user = User.objects.create(email=email,    
-                                    username=username, 
-                                    first_name=first_name, 
-                                    address=address, 
-                                    contact=contact, 
-                                    password=password
-    )
-
-    category_name="testecommerce category"
-    slug= slugify(category_name)
-    category=Category.objects.create(name=category_name, slug=slug)
-
-
-
-    product_name='testproduct'
-    product_description='testproductdescription'
-    product_price=12000
-    product_in_stock_count=4
-    product_owner=test_user
-    product=Product.objects.create(name=product_name, 
-                                description=product_description, 
-                                owner=product_owner, 
-                                category=category, 
-                                price=product_price, 
-                                product_in_stock_count=product_in_stock_count
-    )
-
+    
     def test_product_creation(self):
-        self.assertEquals(self.product.name, "testproduct")
-        self.assertEquals(self.product.category.name, "testecommerce category")
-        self.assertEquals(self.product.description, "testproductdescription")
-        self.assertEquals(self.product.price, 12000)
-        self.assertEquals(self.product.owner.email, "testemail@yahoo.com")
-        self.assertEquals(self.product.product_in_stock_count, 4)
+        test_user = User.objects.create(email=self.email,    
+                                    username=self.username, 
+                                    first_name=self.first_name, 
+                                    address=self.address, 
+                                    contact=self.contact, 
+                                    password=self.password
+        )
+        slug= slugify(self.category_name)
+        category=Category.objects.create(name=self.category_name, slug=slug)
+        colour=Colour.objects.create(name=self.colour)
+
+        product_name='testproduct'
+        product_description='testproductdescription'
+        product_price=12000
+        product_in_stock_count=4
+        product_owner=test_user
+        product=Product.objects.create(name=product_name, 
+                                    description=product_description, 
+                                    owner=product_owner, 
+                                    category=category, 
+                                    price=product_price, 
+                                    product_in_stock_count=product_in_stock_count,
+                                    colour=colour
+        )
+
+        self.assertEquals(product.name, "testproduct")
+        self.assertEquals(product.category.name, "testecommerce category")
+        self.assertEquals(product.description, "testproductdescription")
+        self.assertEquals(product.price, 12000)
+        self.assertEquals(product.owner.email, "testemail@yahoo.com")
+        self.assertEquals(product.product_in_stock_count, 4)
+        self.assertEquals(product.colour, colour)
 
 
 
-    test_user.delete()
-    category.delete()
-    product.delete()
+   
